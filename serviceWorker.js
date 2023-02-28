eab832e0787dac499ac7103e5faaa9f9ef89aaa0
andrew <andrew.santos@watch.tv.br>
2023-02-28 14:15:19 -0300

    modificacoes manifest

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,7 +3,7 @@ const CACHE = "pwabuilder-offline-page";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const offlineFallbackPage = "index.html";
+const offlineFallbackPage = "./offlinepage.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
